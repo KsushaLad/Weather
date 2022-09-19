b2004facf4012b3233f5dbb2ba50eabcf9ccb0d0
Ksenia <ladoshkoksenia@gmail.com>
2022-09-19 17:06:02 +0300

    2

--- app/src/main/java/com/ksusha/weather/MainActivity.kt
+++ app/src/main/java/com/ksusha/weather/MainActivity.kt
@@ -2,14 +2,12 @@ package com.ksusha.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.ksusha.weather.databinding.ActivityMainBinding
-import org.json.JSONObject
+import com.ksusha.weather.fragments.MainFragment
 
 const val API_KEY = "49159399642c44dba8e141734221709"
+val url = "https://api.weatherapi.com/v1/current.json" +
+        "?key=$API_KEY&q=London&aqi=no"
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,22 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bGet.setOnClickListener {
-            getResult("London")
-        }
+        supportFragmentManager.beginTransaction().replace(R.id.placeHolder, MainFragment.newInstance()).commit()
     }
 
-    private fun getResult(name: String) {
-        val url = "https://api.weatherapi.com/v1/current.json" +
-                "?key=$API_KEY&q=$name&aqi=no"
-        val queue = Volley.newRequestQueue(this)
-        val request = StringRequest(Request.Method.GET, url,
-            { response ->
-                val obj = JSONObject(response)
-                val temp = obj.getJSONObject("current") },
-            {}
-        )
-        queue.add(request)
-    }
 
 }
---
+++ app/src/main/java/com/ksusha/weather/viewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.ksusha.weather.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<List<String>>()
+
+}
