ddba2c141122ba14b96751bb8414702051378366
Ksenia <ladoshkoksenia@gmail.com>
2022-10-03 16:07:14 +0300

    9

--- app/src/main/java/com/ksusha/weather/fragments/MainFragment.kt
+++ app/src/main/java/com/ksusha/weather/fragments/MainFragment.kt
@@ -1,23 +1,34 @@
 package com.ksusha.weather.fragments
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.tabs.TabLayoutMediator
 import com.ksusha.weather.adapters.ViewPagerAdapter
 import com.ksusha.weather.databinding.FragmentMainBinding
 import com.ksusha.weather.extencions.isPermissionGranted
 import com.ksusha.weather.model.WeatherModel
+import com.ksusha.weather.objects.DialogManager
 import com.ksusha.weather.utils.*
 import com.ksusha.weather.viewmodel.MainViewModel
 import com.squareup.picasso.Picasso
@@ -30,6 +41,7 @@ class MainFragment : Fragment() {
     private val fragmentList = listOf(HoursFragment.newInstance(), DaysFragment.newInstance())
     private val titleList = listOf(HOURS, DAYS)
     private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,16 +55,60 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
-        requestWeatherData("Kiev")
         updateCurrentCard()
     }
 
+    override fun onResume() {
+        super.onResume()
+        checkLocation()
+    }
+
     private fun init() = with(binding){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
         val adapter = ViewPagerAdapter(activity as FragmentActivity, fragmentList)
         vp.adapter = adapter
         TabLayoutMediator(tabLayout, vp){
             tab, position -> tab.text = titleList[position]
         }.attach()
+        ibSync.setOnClickListener {
+            tabLayout.selectTab(tabLayout.getTabAt(0))
+            checkLocation()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean{
+        val locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun getLocation(){
+        val token = CancellationTokenSource()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, token.token).addOnCompleteListener {
+            requestWeatherData("${it.result.latitude},${it.result.longitude},")
+        }
+    }
+
+    private fun checkLocation(){
+        if (isLocationEnabled()){
+            getLocation()
+        } else {
+            DialogManager.locationSettingsDialog(requireContext(), object : DialogManager.Listener{
+                override fun onClick() {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+
+            })
+        }
     }
 
     private fun updateCurrentCard() = with(binding){
